1d98405cbf99d88fd819a91dfa28dc1f30d21049
Alexander <7086600@gmail.com>
2025-09-29 04:08:05 +0300

    count the number of  first touches is each campaign

--- marketing_attribution.sql
+++ marketing_attribution.sql
@@ -38,3 +38,38 @@ SELECT DISTINCT
     (page_name)
 FROM
     page_visits;
+
+-- What is the user journey?
+-- #3 the number of  first touches is each campaign
+WITH
+    first_touch AS (
+        SELECT
+            user_id,
+            MIN(timestamp) as first_touch_at
+        FROM
+            page_visits
+        GROUP BY
+            user_id
+    ),
+    ft_attr AS (
+        SELECT
+            ft.user_id,
+            ft.first_touch_at,
+            pv.utm_source,
+            pv.utm_campaign
+        FROM
+            first_touch AS 'ft'
+            JOIN page_visits AS 'pv' ON ft.user_id = pv.user_id
+            AND ft.first_touch_at = pv.timestamp
+    )
+SELECT
+    ft_attr.utm_source,
+    ft_attr.utm_campaign,
+    COUNT(*)
+FROM
+    ft_attr
+GROUP BY
+    1,
+    2
+ORDER BY
+    3 DESC;
